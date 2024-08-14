90c8bffbd9a95c66c394f761c63344a56e5c7c86
elishowbr <152816889+elishowbr@users.noreply.github.com>
2024-08-14 11:35:09 -0300

    update 0.4A

--- frontend/script.js
+++ frontend/script.js
@@ -2,10 +2,11 @@ const botao = document.querySelector('#entrar')
 const input = document.querySelector('#login')
 
 const membros = [
-     {id:1, login: "elisson.in", cargo:"Monitor DEV", nome:'Elisson'},
-     {id:2, login: "gustavo.in", cargo:"T.I", nome:'Gusta da T.I'},
-     {id:3, login: "giovanni.in", cargo:"Monitor MKT", nome:'Giovanni'},
-     {id:4, login: "italo.gab.in", cargo:"Monitor DEV", nome:'Italo Gabriel'},
+    {id:1, login: "elisson.in", cargo:"Monitor DEV", nome:'Elisson'},
+    {id:2, login: "gustavo.in", cargo:"T.I", nome:'Gusta da T.I'},
+    {id:3, login: "giovanni.in", cargo:"Monitor MKT", nome:'Giovanni'},
+    {id:4, login: "italo.gab.in", cargo:"Monitor DEV", nome:'Italo Gabriel'},
+    {id:5, login: "lais.in", cargo:"Monitor Design", nome: "Lais Moura"}
 ]
 
 
@@ -13,8 +14,6 @@ input.focus()
 
 function validarMembro(login) {    
 
-    //Reconhece o input e pega seu valor
-    const input = document.querySelector('#login')
     const entrada = login.value
 
     //Reconhece a entrada, e só APÓS isso, conferir o cadastro no banco de dados.
@@ -23,10 +22,7 @@ function validarMembro(login) {
         let usuarioMatch = membros.filter((membro) => {
             return (membro.login == entrada)
         })
-        
-
-
-        
+              
     } else {
         alert('Usuário não válido, tente novamente!')
         input.value = ''
